796212a0e31cc95cd5271a3133d9ddf74fab463d
Patience250 <brownpatience66@gmail.com>
2020-09-01 08:51:28 -0700

    dashboard added

---
+++ UI/js/admin.js
@@ -0,0 +1,28 @@
+let homeSection = document.querySelector('#home-section')
+let navBar = document.querySelector('.nav-links')
+let nav = document.querySelector('nav')
+let navLinks = document.querySelectorAll('a')
+const bars = document.querySelector('.bars')
+const contactForm = document.querySelector('.contact-form')
+window.addEventListener("scroll", () => {
+
+        console.log(window.pageYOffset)
+        if (window.pageYOffset >= 540) {
+            nav.classList.add('change-navbar')
+            navLinks.forEach(navLink => {
+                navLink.classList.add('change-nav-links')
+                bars.style.color = "white"
+            })
+        } else {
+            nav.classList.remove('change-navbar')
+            navLinks.forEach(navLink => {
+                navLink.classList.remove('change-nav-links')
+                bars.style.color = "black"
+            })
+        }
+    })
+    // show & hide nav links
+bars.addEventListener('click', () => {
+    navBar.classList.toggle('show-links')
+
+})
