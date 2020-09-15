c2a082690d07a411da244dac89dfb4e69e4e0b94
Patience250 <brownpatience66@gmail.com>
2020-09-15 09:01:00 -0700

    Read update and delete blog features added

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -5,5 +5,9 @@ const blogController = require('../controllers/blogs')
 
 // route for blog creation
 router.post("/blogs", blogController.createBlog);
+router.get("/blogs", blogController.findBlogs);
+router.get("/blogs/:id", blogController.findBlog);
+router.patch("/blogs/:id", blogController.updatedBlog);
+router.delete("/blogs/:id", blogController.deleteBlog);
 
 module.exports = router
